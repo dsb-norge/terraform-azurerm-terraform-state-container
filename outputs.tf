54dbcf3288d43cc7ad42afeed6927192eda11eea
artlvns <artjoms.laivins@dsb.no>
2025-03-06 10:08:49 +0100

    feat!: Increase storage account security and reliability by * Disabling shared access keys. * Enabling default OAUTH authentication. * Dissalow nested items to be public. * Adding blob properties for change feed, access time and versioning.

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,8 @@ output "container_name" {
   description = "Name of the storage container created for terraform backend state."
   value       = azurerm_storage_container.tfstate.name
 }
+#TODO: his property will be removed in v5 of the provider, and users are expected to directly use azurerm_storage_container.example.id. Though the id of azurerm_storage_container will change from the form of "https://example.blob.core.windows.net/container" to the resource manager id, only when the resource is created using the new storage_account_id. This means in your existing workspace, the id won't work.
+#See https://github.com/hashicorp/terraform-provider-azurerm/pull/27733 for details.
 output "container_resource_manager_id" {
   description = "The Resource Manager ID of the storage container created for terraform backend state."
   value       = azurerm_storage_container.tfstate.resource_manager_id
